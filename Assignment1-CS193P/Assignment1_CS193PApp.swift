b55483a839a5a12cda83159cfe4abb0237c92a7f
Margo Naumenko <imaggi99@mail.ru>
2021-07-25 12:39:56 +0300

    Initial Commit

---
+++ Assignment1-CS193P/Assignment1_CS193PApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment1_CS193PApp.swift
+//  Assignment1-CS193P
+//
+//  Created by Margo Naumenko on 25.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct Assignment1_CS193PApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment1-CS193P/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Assignment1-CS193P
+//
+//  Created by Margo Naumenko on 25.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
